6243b407b16dcbe9a4e6f9629ec2e86c0561022b
ckswhd7774 <ckswhd7774@gmail.com>
2021-06-29 20:19:32 +0900

    feat:create social login

---
+++ studycafe/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class StudyCafe(models.Model) :
+    businessuser = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+    address = models.TextField()
+    price_per_hour = models.CharField
+    business_hour_start = models.CharField(max_length=32)
+    business_hour_end = models.CharField(max_length=32)

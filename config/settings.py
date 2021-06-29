bbcba9f4f1fa8673a405eb093280cd047cfdbdad
ckswhd7774 <ckswhd7774@gmail.com>
2021-06-29 19:42:14 +0900

    feat:create social login

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iiv2-r-9(k!=#yr(or=!3p1$if+*695*#2o7%#cxgoah0e0!!p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

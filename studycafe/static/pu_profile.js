c5085dcb9616abe77566ef0bb91e92da729623de
Eunbi Choi <eunbichoi1@gmail.com>
2021-07-30 04:02:14 +0900

    feat: submit button toggle for input event
    Submit button toggle for input event completed on Edit Profile Tab

--- studycafe/static/pu_profile.js
+++ studycafe/static/pu_profile.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("personal profile page connected!")
-   
-// profile page tab toggling
-      
-    
+  console.log("personal profile page connected!")
+
+  //activate submit button for input value changes for EDIT PROFILE TAB
+  $nameInput = document.querySelector('#profile-name')
+  $usernameInput = document.querySelector('#profile-username')
+  $emailInput = document.querySelector('#profile-email')
+  $submitButton = document.querySelector('.profile-edit-submit')
+
+  $nameInput.addEventListener('change', toggleSubmit);
+  $usernameInput.addEventListener('change', toggleSubmit);
+  $emailInput.addEventListener('change', () => {
+    toggleSubmit()
   });
+  function toggleSubmit(){
+    $submitButton.classList.remove('disabled');
+    $submitButton.disabled = false;
+  };
+
+});

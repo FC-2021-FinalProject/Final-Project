09ce81f0c356e735f810497d695adfe49ddd996f
Eunbi Choi <eunbichoi1@gmail.com>
2021-07-30 16:43:46 +0900

    feat: profile page tab option toggling

--- studycafe/static/pu_profile.js
+++ studycafe/static/pu_profile.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("personal profile page connected!")
 
-  //activate submit button for input value changes for EDIT PROFILE TAB
+//profile edit page -- submit button toggling
+
+  //FORM ITEMS
   $nameInput = document.querySelector('#profile-name')
   $usernameInput = document.querySelector('#profile-username')
   $emailInput = document.querySelector('#profile-email')
@@ -16,5 +18,100 @@ document.addEventListener("DOMContentLoaded", () => {
     $submitButton.classList.remove('disabled');
     $submitButton.disabled = false;
   };
+
+// profile page tab event
+
+  //TABS
+  const $editProfileTab = document.querySelector('.pu-profile-header');
+  const $changePasswordTab = document.querySelector('.pu-password-header');
+  const $reservationsTab = document.querySelector('.pu-reservations-header');
+  const $bookmarkedCafesTab = document.querySelector('.pu-bookmarked-header');
+
+  //CONTENTS
+  const $editProfile = document.querySelector('.pu-profile-contents');
+  const $changePassword = document.querySelector('.pu-password-contents');
+  const $reservations = document.querySelector('.pu-reservations-contents');
+  const $bookmarkedCafes = document.querySelector('.pu-bookmarked-contents');
+
+  //EVENTS
+  $editProfileTab.addEventListener('click', () => {
+    if ($editProfileTab.classList.contains('selected')) {
+      console.log("already selected!")
+    } else { 
+        $editProfileTab.classList.add('selected')
+        $editProfile.classList.remove('visually-hidden')
+        
+        if ($changePasswordTab.classList.contains('selected')){
+          $changePasswordTab.classList.remove('selected')
+          $changePassword.classList.add('visually-hidden')
+        } else if ($reservationsTab.classList.contains('selected')){
+          $reservationsTab.classList.remove('selected')
+          $reservations.classList.add('visually-hidden')
+        } else if ($bookmarkedCafesTab.classList.contains('selected')){
+          $bookmarkedCafesTab.classList.remove('selected')
+          $bookmarkedCafes.classList.add('visually-hidden')
+        }
+      }
+  });
+
+  $changePasswordTab.addEventListener('click', () => {
+    if ($changePasswordTab.classList.contains('selected')) {
+      console.log("already selected!")
+    } else { 
+        $changePasswordTab.classList.add('selected')
+        $changePassword.classList.remove('visually-hidden')
+        
+        if ($editProfileTab.classList.contains('selected')){
+          $editProfileTab.classList.remove('selected')
+          $editProfile.classList.add('visually-hidden')
+        } else if ($reservationsTab.classList.contains('selected')){
+          $reservationsTab.classList.remove('selected')
+          $reservations.classList.add('visually-hidden')
+        } else if ($bookmarkedCafesTab.classList.contains('selected')){
+          $bookmarkedCafesTab.classList.remove('selected')
+          $bookmarkedCafes.classList.add('visually-hidden')
+        }
+      }
+  });
+
+  $reservationsTab.addEventListener('click', () => {
+    if ($reservationsTab.classList.contains('selected')) {
+      console.log("already selected!")
+    } else { 
+        $reservationsTab.classList.add('selected')
+        $reservations.classList.remove('visually-hidden')
+        
+        if ($editProfileTab.classList.contains('selected')){
+          $editProfileTab.classList.remove('selected')
+          $editProfile.classList.add('visually-hidden')
+        } else if ($changePasswordTab.classList.contains('selected')){
+          $changePasswordTab.classList.remove('selected')
+          $changePassword.classList.add('visually-hidden')
+        } else if ($bookmarkedCafesTab.classList.contains('selected')){
+          $bookmarkedCafesTab.classList.remove('selected')
+          $bookmarkedCafes.classList.add('visually-hidden')
+        }
+      }
+  });
+
+  $bookmarkedCafesTab.addEventListener('click', () => {
+    if ($bookmarkedCafesTab.classList.contains('selected')) {
+      console.log("already selected!")
+    } else { 
+        $bookmarkedCafesTab.classList.add('selected')
+        $bookmarkedCafes.classList.remove('visually-hidden')
+        
+        if ($editProfileTab.classList.contains('selected')){
+          $editProfileTab.classList.remove('selected')
+          $editProfile.classList.add('visually-hidden')
+        } else if ($changePasswordTab.classList.contains('selected')){
+          $changePasswordTab.classList.remove('selected')
+          $changePasswordTab.classList.add('visually-hidden')
+        } else if ($reservationsTab.classList.contains('selected')){
+          $reservationsTab.classList.remove('selected')
+          $reservations.classList.add('visually-hidden')
+        }
+      }
+  });
 
 });

18cf75dd858a7f7492fd29797595b96f029737b7
Eunbi Choi <eunbichoi1@gmail.com>
2021-07-31 04:40:29 +0900

    edit: drop-menu clickable area + index layout/style
    (header-nav.html)
    expanded drop-down menu close area from open button to whole body
    
    (index.html)
    -temporary fix: commented out random logic for choosing cafes so that
    cafelist queries all
    -layout and styling for cafelist

--- studycafe/views.py
+++ studycafe/views.py
@@ -29,8 +29,8 @@ SUCCESS_MSG = {
     'PROFILE_UPDATED': 'Profile updated successfully.',
 }
 def index(request):
-    # cafes = StudyCafe.objects.all()
-
+    cafes = StudyCafe.objects.all()
+    # print(cafes)
     # random_index = random.sample(range(0,len(cafes)), 4)
 
     # random_cafes = []
@@ -38,8 +38,8 @@ def index(request):
     #     print(i)
     #     random_cafes.append(cafes[random_index[i]])
 
-    # context = {'cafelists': random_cafes}
-    return render(request, 'index.html')
+    context = {'cafelists': cafes}
+    return render(request, 'index.html', context)
 
 # function for email authentication
 def verified_callback(user):
